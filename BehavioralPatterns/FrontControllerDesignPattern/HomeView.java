2cc5bd8d913e34e14fdb702bd1d3afbf08a0f658
adroit18 <consummatelydone@gmail.com>
2021-01-02 11:48:07 +0000

    Front Controller Pattern

---
+++ BehavioralPatterns/FrontControllerDesignPattern/HomeView.java
@@ -0,0 +1,7 @@
+package BehavioralPatterns.FrontControllerDesignPattern;
+
+public class HomeView {
+   public void show(){
+      System.out.println("Displaying Home Page");
+   }
+}
---
+++ BehavioralPatterns/FrontControllerDesignPattern/StudentView.java
@@ -0,0 +1,7 @@
+package BehavioralPatterns.FrontControllerDesignPattern;
+
+public class StudentView {
+   public void show(){
+      System.out.println("Displaying Student Page");
+   }
+}
